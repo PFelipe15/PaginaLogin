9e8fcbeb4e7ae869b97fa1d01255d0e98cee8328
Paulo Felipe <PauloFelipet15@gmail.com>
2023-03-20 10:31:42 -0300

    hidden and visible eyes in password is sucessful

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function eyeOpen() {
-let data = document.querySelector('.')
+function heandleEyeToClose() {
+
+    let eyeNone = document.querySelector('.eye-none')
+    let eyeOpen = document.querySelector('.eye-open')
+    eyeNone.style.visibility = 'hidden'
+    eyeOpen.style.visibility = 'visible'
+    document.querySelector('.passwordEye').type = "text"
+}
+
+
+function heandleEyeToOpen() {
+
+
+    let eyeNone = document.querySelector('.eye-none')
+    let eyeOpen = document.querySelector('.eye-open')
+    eyeNone.style.visibility = 'visible'
+    eyeOpen.style.visibility = 'hidden'
+    document.querySelector('.passwordEye').type = "password"
 }
